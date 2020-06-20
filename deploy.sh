f50f2a8a593c4377eb6ca1c80ef0899058a6a5dc
Phil Corbett <corbett.phil@gmail.com>
2020-06-20 12:41:36 -0400

    Remove some old stuff related to the API layer project. (#38)

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,12 @@ dotnet clean
 
 # build a new version of core
 docker build -f Db.Dockerfile -t tete-db-img .
-docker build -f Api.Dockerfile -t tete-api-img .
 docker build -f Web.Dockerfile -t tete-web-img .
 
 docker tag tete-db-img:latest puremunky/tete-db:latest
-docker tag tete-api-img:latest puremunky/tete-api:latest
 docker tag tete-web-img:latest puremunky/tete-web:latest
 
 docker push puremunky/tete-db:latest
-docker push puremunky/tete-api:latest
 docker push puremunky/tete-web:latest
 
 # kubectl apply -f tete-deployment.yml
--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 # stop existing container
-docker stop tete-api
 docker stop tete-web
 docker stop tete-db
 
 # remove existing container
-docker rm tete-api
 docker rm tete-web
 docker rm tete-db
 
@@ -12,18 +10,16 @@ docker rm tete-db
 dotnet clean
 
 #docker run --name tete-db -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=tetePassword!' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-CU8-ubuntu
-docker run -dit --name tete-db -p 1433:1433 tete-db-img
+#docker run -dit --name tete-db -p 1433:1433 tete-db-img
 sleep 5
 
 #dotnet ef database update -p Tete.Api
 
 # build a new version of core
-docker build -f Api.Dockerfile -t tete-api-img .
 docker build -f Web.Dockerfile -t tete-web-img .
 docker build -f Db.Dockerfile -t tete-db-img .
 
 # run core app
-docker run -dit --name tete-api -p 80:80 tete-api-img 
 docker run -dit --name tete-web -p 80:80 tete-web-img
 docker run -dit --name tete-db -p 1433:1433 tete-db-img
 
