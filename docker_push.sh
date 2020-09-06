2d3ab8de02664298e0b8d5ee8fd75c8ae95e5127
Phil Corbett <corbett.phil@gmail.com>
2020-09-06 14:19:10 -0400

    Feature/support (#53)
    * updated gitignore to remove log files
    
    * add some bootstrap to make things prettier.
    
    * card styling.
    
    * Updated mentorship styles and flow.
    
    * Add topic component styles
    
    * WIP: Update docker setup.
    
    * K8s updates.
    
    * styling cards and such
    
    * WIP: made a new tile component
    
    * tete tile
    
    * Styling and bug fixes.
    
    * Update support text
    
    * session changes and link admin.
    
    * created a global loader.
    
    * Global loader and service.
    
    * Error handling and updated authentication. Somethings might be broken.
    
    * Added logService to ControllerRoot and ServiceBase updated controllers and services to use this structure.
    
    * HTTPS setup start.
    
    * fixed the docker build.
    
    * Update k8s deployment script.
    
    * Travis build and docker push?
    
    * update deploy script
    
    * update docker tagging

--- docker_push.sh
+++ docker_push.sh
@@ -4,8 +4,8 @@ echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -f Db.Dockerfile -t tete-db-img .
 docker build -f Web.Dockerfile -t tete-web-img .
 
-docker tag tete-db-img:$TRAVIS_BUILD_NUMBER puremunky/tete-db:$TRAVIS_BUILD_NUMBER
-docker tag tete-web-img:$TRAVIS_BUILD_NUMBER puremunky/tete-web:$TRAVIS_BUILD_NUMBER
+docker tag tete-db-img:latest puremunky/tete-db:$TRAVIS_BUILD_NUMBER
+docker tag tete-web-img:latest puremunky/tete-web:$TRAVIS_BUILD_NUMBER
 
 docker push puremunky/tete-db:$TRAVIS_BUILD_NUMBER
 docker push puremunky/tete-web:$TRAVIS_BUILD_NUMBER
