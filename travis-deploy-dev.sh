e3c6e0b8cf9d45e48af5dc4364605782c8edb92f
Phil Corbett <corbett.phil@gmail.com>
2020-09-09 18:45:51 -0400

    Feature/prod readiness (#59)
    * updated gitignore to remove log files
    
    * setup deploy pipelines
    
    * add some deployment todos
    
    * Clean up stuff. Begin certificate work.
    
    * Update deployment scripts, reseting password. Deploys to dev.
    
    * Add logging to many services.
    
    * Add admin dashboard with some stats.
    
    * Auto deployment?
    
    * Remove cert copy for build testing.
    
    * Remove cert again.

--- travis-deploy-dev.sh
+++ travis-deploy-dev.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # TODO: Push to dev k8s cluster.
+# https://gist.github.com/theraaz/933ab38f1dcbd985f448439728bfef6e
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
 docker tag tete-web-img:latest puremunky/tete-web:build-$TRAVIS_BUILD_NUMBER
 
 docker push puremunky/tete-web:build-$TRAVIS_BUILD_NUMBER
+
+envsubst <./deployments/do-dev.yml >./deployments/do-dev.yml.out
+mv ./deployments/do-dev.yml.out ./deployments/do-dev.yml
+kubectl --kubeconfig=/dev/null --token=$KUBERNETES_TOKEN --server=$KUBERNETES_SERVER --certificate-authority=cert.crt apply -f ./deployments/do-dev.yml
