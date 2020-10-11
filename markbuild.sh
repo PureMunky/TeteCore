4e3edffdb4ae0012f6ce8894c95bfcad376d7d49
Phil Corbett <corbett.phil@gmail.com>
2020-10-11 07:18:44 -0400

    Fixed build error.

--- markbuild.sh
+++ markbuild.sh
@@ -1,16 +1,3 @@
 echo "Time: $(date -Iseconds)" > ./Tete.Web/wwwroot/build.txt
-
-if [ -z "$TRAVIS_BUILD_NUMBER" ]
-then
-  echo "Build: local" >> ./Tete.Web/wwwroot/build.txt
-else
-  echo "Build: $TRAVIS_BUILD_NUMBER" >> ./Tete.Web/wwwroot/build.txt
-fi
-
-
-if [ -z "$TRAVIS_BRANCH" ]
-then
-  echo "Branch: n/a" >> ./Tete.Web/wwwroot/build.txt
-else
-  echo "Branch: $TRAVIS_BRANCH" >> ./Tete.Web/wwwroot/build.txt
-fi
+echo "Build: $TRAVIS_BUILD_NUMBER" >> ./Tete.Web/wwwroot/build.txt
+echo "Branch: $TRAVIS_BRANCH" >> ./Tete.Web/wwwroot/build.txt
