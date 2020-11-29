e3bff4f232913ab2bae29f252a08eb665fa42d8c
Phil Corbett <corbett.phil@gmail.com>
2020-11-29 07:00:08 -0500

    Fixed some issues with hard coded values for my old computer.

--- run-db.sh
+++ run-db.sh
@@ -14,7 +14,8 @@ docker rm tete-db
 # docker build -f Db.Dockerfile -t tete-db-img .
 
 # run docker image
-docker run -dit --name tete-db -p 1433:1433 -v E:\\Projects\\Tete\\Database:/var/opt/mssql -e ACCEPT_EULA=Y -e MSSQL_PID=Express -e SA_PASSWORD=$teteDBPassword mcr.microsoft.com/mssql/server:2017-CU8-ubuntu
+#cmd //c "docker run -dit --name tete-db -p 1433:1433 -v ${PWD}/DB:/var/opt/mssql -e ACCEPT_EULA=Y -e MSSQL_PID=Express -e SA_PASSWORD=$teteDBPassword mcr.microsoft.com/mssql/server:2017-CU8-ubuntu"
+docker run -dit --name tete-db -p 1433:1433 -e ACCEPT_EULA=Y -e MSSQL_PID=Express -e SA_PASSWORD=$teteDBPassword mcr.microsoft.com/mssql/server:2017-CU8-ubuntu
 
 # create database migration
 # cd Tete.Web && dotnet-ef migrations add build
